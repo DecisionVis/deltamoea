ee1b0a392b1ab1c76562b7dcad6af3e03d875671
Matthew Woodruff <mwoodruff@decisionvis.com>
2016-12-20 16:55:58 -0500

    Visualizer: start cleaning up

--- evaluation/visualize_pm.py
+++ evaluation/visualize_pm.py
@@ -16,8 +16,7 @@ class HelloTriangle(QOpenGLWidget): #rename to PMHistogram
         self.shader_program = QOpenGLShaderProgram(self)
     def initializeGL(self):
         self.vp.setVersion(2,1)
-        # rename to vertex_program_text
-        shader_program_text = """
+        vertex_program_text = """
         #version 130
         in vec3 position;
 
@@ -33,16 +32,14 @@ class HelloTriangle(QOpenGLWidget): #rename to PMHistogram
             gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
         }
         """
-        self.shader_program.create() # try to remove?
-        self.shader_program.bind() # try to remove?
         self.shader_program.addShaderFromSourceCode(
             QOpenGLShader.Vertex,
-            shader_program_text)
+            vertex_program_text)
         self.shader_program.addShaderFromSourceCode(
             QOpenGLShader.Fragment,
             fragment_shader_text)
-        self.shader_program.link()
-        self.shader_program.bind() # try to remove?
+        self.shader_program.link() # Necessary
+        self.shader_program.bind() # Necessary
         vertices = [ # replace with ndarray
             QVector3D(-0.5, 0, 0),
             QVector3D( 0.5, 0, 0),
@@ -51,11 +48,11 @@ class HelloTriangle(QOpenGLWidget): #rename to PMHistogram
             'position', vertices)
     def resizeGL(self, width, height):
         fun = QOpenGLContext.currentContext().versionFunctions(self.vp)
-        fun.glClearColor(0.2, 0.3, 0.3, 1.0) # choose a less ugly color
+        fun.glClearColor(1.0, 1.0, 1.0, 1.0)
         fun.glClear(fun.GL_COLOR_BUFFER_BIT)
     def paintGL(self):
         fun = QOpenGLContext.currentContext().versionFunctions(self.vp)
-        fun.glClearColor(0.2, 0.3, 0.3, 1.0) # choose a less ugly color
+        fun.glClearColor(1.0, 1.0, 1.0, 1.0)
         fun.glClear(fun.GL_COLOR_BUFFER_BIT)
         self.shader_program.enableAttributeArray('position')
         fun.glDrawArrays(fun.GL_TRIANGLES, 0, 3)
