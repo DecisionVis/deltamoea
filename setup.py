3393a9a194f182ce22ba1013de1056fd560fddce
Matthew Woodruff <sevensor@gmail.com>
2017-07-11 21:53:13 -0400

    Distribution (U): finish writing setup.py
    I've specified "final" stability but put the version in as 0.90.
    I'm not going to upload until 1.00 at any rate.

--- setup.py
+++ setup.py
@@ -4,8 +4,33 @@ setup.py: Loosely cribbed from the example in pypa
 https://github.com/pypa/sampleproject/blob/master/setup.py
 """
 from setuptools import setup
-from setuptools import find_packages
 from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding='utf-8') as fp:
+    long_description = fp.read()
+
+setup(
+    name="deltaMOEA",
+    version='0.90',
+    description='Multi-Objective Grid Search Algorithm',
+    long_description=long_description,
+    url='https://bitbucket.org/decisionvis/dmoea-python',
+    author="DecisionVis LLC",
+    author_email="team@decisionvis.com",
+    license="BSD",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords=[
+        "δMOEA", "MOEA", "grid search", "optimization", "multi-objective"],
+    packages=['dmoea'],
+)
+
